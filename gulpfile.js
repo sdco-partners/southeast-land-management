f320f3da11b6505f7d72483cfe78b06eca0ce8a8
Asif Ahmed <asif@stitchdesignco.com>
2017-02-24 14:45:30 -0500

    Add livereload, plumber, and notify to gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,11 @@ var gulp = require("gulp")
 , sass = require("gulp-sass")
 , uglify = require("gulp-uglify")
 , concat = require('gulp-concat')
-, livereload = require('gulp-livereload')
 , clean = require('gulp-clean-css')
-, sourcemaps = require('gulp-sourcemaps');
+, sourcemaps = require('gulp-sourcemaps')
+, plumber = require('gulp-plumber')
+, notify = require('gulp-notify')
+, livereload = require('gulp-livereload');
 
 
 // uri
@@ -18,12 +20,22 @@ var paths = {
 	dest: uri.concat('prod/')
 }
 
+// Error Handling
+var plumberErrorHandler = {
+	errorHandler: notify.onError({
+		title: 'Gulp',
+		message: 'Error. <%= error.message %>'
+	})
+};
+
 // Sass to Css-min
 gulp.task('styles', function() {
 	gulp.src(paths.styles)
-	  .pipe(sass().on('error', sass.logError))
+	  .pipe(plumber(plumberErrorHandler))
+	  .pipe(sass())
     .pipe(clean())
-	  .pipe(gulp.dest(paths.dest));
+	  .pipe(gulp.dest(paths.dest))
+	  .pipe(livereload());
 });
 
 // Minify Css 
@@ -32,19 +44,23 @@ gulp.task('minify', function() {
     .pipe(sourcemaps.init())
     .pipe(clean())
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest(paths.dest));
+    .pipe(gulp.dest(paths.dest))
+    .pipe(livereload());
 });
 
 // Uglify JS
 gulp.task('uglify', function() {
 	gulp.src(paths.js)
+		.pipe(plumber(plumberErrorHandler))
 		.pipe(concat('scripts-min.js'))
 	  .pipe(uglify())
 	  .pipe(gulp.dest(paths.dest));
+	  .pipe(livereload());
 });
 
 // Watch task
 gulp.task('default', function() {
+	livereload.listen();
 	gulp.watch(paths.scss, ['styles']);
 	gulp.watch(paths.js, ['uglify']);
 	gulp.watch(paths.dest, ['minify']);
