04ad50a4ac1c7e3861d13b3d270d43dec8cb6e6a
Asif Ahmed <asif@stitchdesignco.com>
2017-03-01 09:53:29 -0500

    Add mixing for background image settings

--- content/themes/slmgmt/components/bio.php
+++ content/themes/slmgmt/components/bio.php
@@ -4,9 +4,7 @@
   */ 
 ?>
 
-<img id="" class=""
-    src="<?php echo $GLOBALS['url']."/assets/img-01.png" ?>" 
-    alt="">
+<div class="image"></div>
 <div class="copy">
   <p class="">
   	In 2016, <span class="underline">South Street Partners</span> formed Southeast Land Management to oversee a ~19,000 acre land portfolio. The portfolio consists of 39 separate properties located in desirable market throughout North Carolina, South Carolina, Tennessee and Texas. SSP Southeast Land Management is actively seeking to maximize value for existing investors through marketing the properties to homebuilders, developers, and other real estate investors.
