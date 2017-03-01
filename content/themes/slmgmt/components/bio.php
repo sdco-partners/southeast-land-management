ba59d474823ee34ed0feded0dce41bb5af4b7948
Asif Ahmed <asif@stitchdesignco.com>
2017-03-01 09:21:22 -0500

    add flex row to bio section in Index

--- content/themes/slmgmt/components/bio.php
+++ content/themes/slmgmt/components/bio.php
@@ -2,13 +2,14 @@
   /**
   * Section =>  About
   */ 
-
 ?>
 
-<img class="" id="" src="" alt="">
+<img id="" class=""
+    src="<?php echo $GLOBALS['url']."/assets/img-01.png" ?>" 
+    alt="">
 <div class="copy">
   <p class="">
-  	In 2016, <span>South Street Partners</span> formed Southeast Land Management to oversee a ~19,000 acre land portfolio. The portfolio consists of 39 separate properties located in desirable market throughout North Carolina, South Carolina, Tennessee and Texas. SSP Southeast Land Management is actively seeking to maximize value for existing investors through marketing the properties to homebuilders, developers, and other real estate investors.
+  	In 2016, <span class="underline">South Street Partners</span> formed Southeast Land Management to oversee a ~19,000 acre land portfolio. The portfolio consists of 39 separate properties located in desirable market throughout North Carolina, South Carolina, Tennessee and Texas. SSP Southeast Land Management is actively seeking to maximize value for existing investors through marketing the properties to homebuilders, developers, and other real estate investors.
   </p>
   <a class="" href="">
   	Meet the team
--- content/themes/slmgmt/components/main.php
+++ content/themes/slmgmt/components/main.php
@@ -5,7 +5,7 @@
 
 ?>
 
-<h1 id="thisone">
+<h1>
   Southeast Land Management
 </h1>
 <h2>
@@ -13,5 +13,8 @@
 </h2>
 <div class="arrow down">
 </div>
-<div id="slider-albert">
+<div id="sldr-albert">
+  <img id="sa-1" class="albert" 
+    src="<?php echo $GLOBALS['url']."/assets/sldr-albert-04.png" ?>" 
+    alt="">
 </div>
