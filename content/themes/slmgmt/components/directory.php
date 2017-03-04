7dec06dd1804a321c697453be4d488d01332e1f7
Asif Ahmed <asif@stitchdesignco.com>
2017-03-04 12:07:11 -0500

    Build styles for writeup sections

--- content/themes/slmgmt/components/directory.php
+++ content/themes/slmgmt/components/directory.php
@@ -5,37 +5,37 @@
 
 ?>
 
-<div class="list">
+<div class="list" id="list-melton">
 	<h2>Patrick Melton</h2>
 	<p>Managing Partners</p>
 </div>
 
-<div class="list" id="randolph">
+<div class="list" id="list-randolph">
 	<h2>Chris Randolph</h2>
 	<p>Partner</p>
 </div>
 
-<div class="list" id="coblentz">
+<div class="list" id="list-coblentz">
 	<h2>Scott Coblentz</h2>
 	<p>President</p>
 </div>
 
-<div class="list" id="armistead">
+<div class="list" id="list-armistead">
 	<h2>Henry Armistead</h2>
 	<p>Senior Associate</p>
 </div>
 
-<div class="list" id="phillips">
+<div class="list" id="list-phillips">
 	<h2>Jordan Phillips</h2>
 	<p>Partner</p>
 </div>
 
-<div class="list" id="culp">
+<div class="list" id="list-culp">
 	<h2>Will Culp</h2>
 	<p>Partner</p>
 </div>
 
-<div class="list" id="cornwell">
+<div class="list" id="list-cornwell">
 	<h2>Bennette Cornwell</h2>
 	<p>Investor Relations Manager and Accountant</p>
 </div>  
