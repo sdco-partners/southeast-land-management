c2245ac1a8b108b6e24289b077668f8fa1176bd6
Asif Ahmed <asif@stitchdesignco.com>
2017-03-15 12:28:28 -0400

    add WP Query to Explore.php and loop through sub markets

--- content/themes/slmgmt/components/explore.php
+++ content/themes/slmgmt/components/explore.php
@@ -3,11 +3,34 @@
   * Section =>  Explore
   */ 
 
+  // add url query 
+  esc_url( add_query_arg('market', '') );
+  $args = array(
+     'taxonomy' => 'market',
+     'hide_empty' => false          
+  );
 ?>
+<h2>
 
-<h2><?php the_field('explore_copy', 11); ?></h2>
+	<?php
+	  $markets = get_terms( $args );
+    $total_count = count($markets);
+    $count = 1;
+	  foreach ( $markets as $market ) :
+	?>
+	    <a href="<?php echo $GLOBALS['root']?>/communities?market=<?php echo $market->slug;?>">
+	      <?php echo $market->name;?>
+	      	
+	      </a>
+	<?php  if( $count < $total_count ){
+        echo ' / ';
+	    }
+	  $count++;endforeach; 
+	?>
+
+</h2>
 <p>
-  <a href="<?php the_field('explore_link', 11); ?>">
-    <?php the_field('explore_cta', 11); ?>
+  <a href="<?php echo GLOBALS['root'] ?>/about">
+    Explore Our Community
   </a>
 </p>
--- content/themes/slmgmt/explore.php
+++
@@ -1,9 +0,0 @@
-<?php
-  /**
-  * Section =>  Explore
-  */ 
-
-?>
-
-<h2>Charlotte / Raleigh / Charleston / Nashville / Austin /  San Antonio</h2>
-<p>Explore Our Community</p>
